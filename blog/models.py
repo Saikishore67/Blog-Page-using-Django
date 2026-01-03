d876d4e97b6c0e2900358b487bc751f294bc8d3e
Sai Kishore Saravanan <saravanansaikishore67@gmail.com>
2026-01-03 20:53:48 +0530

    Fix migrations and update models in vscode 03-01-2026

--- blog/models.py
+++ blog/models.py
@@ -5,5 +5,5 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE) # the newly made changes
     date_posted = models.DateTimeField(auto_now_add = True)
